4c9d8d0285aa260181d3993d7f9850634fcfa923
Asmaul Hasnat <30300216+asmaulhasnat@users.noreply.github.com>
2021-04-29 20:53:45 +0600

    remove same id

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -17,12 +17,12 @@
                        id="username" type="text" placeholder="Enter your name">
             </div>
             <div class="mb-4">
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
                     Email
                 </label>
                 <input name="email"  v-model='email'
                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                       id="username" type="email" placeholder="Enter your email">
+                       id="email" type="email" placeholder="Enter your email">
             </div>
             <div class="mb-6">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="password" >
@@ -36,7 +36,7 @@
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="password2" >
                     Confirm Password
                 </label>
-                <input name="password" v-model='password_confirmation'
+                <input name="password_confirmation" v-model='password_confirmation'
                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                        id="password2" type="password" placeholder="******************">
             </div>
